2001854b1304cf07f9e126227aeae0d23e696dc1
D.F. <df@dfder.tw>
2025-02-03 01:21:16 +0800

    feat: deploy my github pages

---
+++ docs/assets/js/main.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('header h1');
+    if (header) {
+        header.textContent += ' - Tech & Project Showcase';
+    }
+});
